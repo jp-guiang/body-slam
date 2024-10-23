a21e7e0db36d2aa56e919877c77eeca5147d522b
JP Guiang <jpguiang15@gmail.com>
2024-10-23 22:04:50 +1300

    feat: include move data into move page

--- src/pages/[identifier]/index.tsx
+++ src/pages/[identifier]/index.tsx
@@ -1,11 +1,43 @@
 import type {NextPage} from "next"
 import { useRouter } from "next/router";
+import moves from "../../data/moves.json";
+import snorlax from "../../../public/snorlax.webp";
+import Head from "next/head";
+import Image from "next/image";
 
 const Move: NextPage = () => {
     const router = useRouter()
     const { query } = router
 
-    return <h1>Test {query.identifier} </h1>
+    const move = moves.find(move => move.identifier === query.identifier)
+
+    return (
+        <>
+            <Head>
+                <title>Body Slam</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
+                <div className="flex">
+                    <Image
+                        src={snorlax}
+                        alt="Snorlax image"
+                        height={513}
+                        className="pr-12"
+                    />
+                    <div>
+                        <p>Name: {move?.name}</p>
+                        <p>Type: {move?.type}</p>
+                        <p>Damage: {move?.damage}</p>
+                        <p>PP: {move?.pp}</p>
+                        <p>Potential Damage: {move?.damage * move?.pp}</p>
+                    </div>
+                    
+                </div>
+            </main>
+        </> 
+    )
 }
 
 export default Move
