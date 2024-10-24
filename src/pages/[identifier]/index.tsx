7ba8b06b1201084d9b8c41f4c3109a21433e76df
JP Guiang <jpguiang15@gmail.com>
2024-10-24 14:38:23 +1300

    feat and refactor: add type colouring and add not found page for non existent moves

--- src/pages/[identifier]/index.tsx
+++ src/pages/[identifier]/index.tsx
@@ -1,46 +1,77 @@
-import type {NextPage} from "next"
-import { useRouter } from "next/router";
+import React from "react";
+import type { NextPage } from "next";
+import { notFound } from "next/navigation";
 import moves from "../../data/moves.json";
 import snorlax from "../../../public/snorlax.webp";
 import Image from "next/image";
+import typeColours from "../../data/type-colours";
+import type { Move } from "../../types/moves";
 
-const Move: NextPage = () => {
-    const router = useRouter()
-    const { query } = router
+export async function getServerSideProps(context: { params: { identifier: string } }) {
+    const { identifier } = context.params;
 
-    const move = query.identifier ? moves.find(move => move.identifier === query.identifier) : null;
-  
-    if (!move) {
-      return <p>Loading...</p>;
+    const move = moves.find(move => move.identifier === identifier) || null;
+
+    if(!move) {
+        notFound();
     }
 
-    const imagePath  = move.image ? `/moves/${move.image}` : snorlax;
+    return {
+        props: {
+            move,
+        },
+    };
+}
+
+const Move: NextPage<{ move: Move }> = ({ move }) => {
+
+    const imagePath = move.image ? `/moves/${move.image}` : snorlax;
+    const typeColour = move.type ? typeColours[move.type] : '#f1e5d7';
 
     return (
         <>
             <main className="container mx-auto flex flex-col justify-center p-4">
-                <div className="flex">
+                <div className="flex justify-between items-center ">
                     <div className="w-1/2 relative h-64">
                         <Image
                             src={imagePath}
-                            alt="Snorlax image"
+                            alt={move.name || "Move image"}
                             fill
                             style={{ objectFit: 'contain' }}
                             className="w-12 pr-12"
                         />
                     </div>
                     
-                    <div>
-                        <p>Name: {move?.name}</p>
-                        <p>Type: {move?.type}</p>
-                        <p>Damage: {move?.damage}</p>
-                        <p>PP: {move?.pp}</p>
-                        <p>Potential Damage: {move?.damage * move?.pp}</p>
+                    <div className="w-1/2 rounded-lg border-8" style={{ borderColor: typeColour }}>
+                        <table className="table-fixed w-full">
+                            <tbody>
+                                <tr>
+                                    <td className="p-2 uppercase">Name</td>
+                                    <td className="capitalize">{move.name}</td>
+                                </tr>
+                                <tr>
+                                    <td className="p-2 uppercase">Type</td>
+                                    <td className="capitalize">{move.type}</td>
+                                </tr>
+                                <tr>
+                                    <td className="p-2 uppercase">Damage</td>
+                                    <td className="capitalize">{move.damage}</td>
+                                </tr>
+                                <tr>
+                                    <td className="p-2 uppercase">PP</td>
+                                    <td className="capitalize">{move.pp}</td>
+                                </tr>
+                                <tr>
+                                    <td className="p-2 uppercase">Damage Potential</td>
+                                    <td className="capitalize">{move.damage * move.pp}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </main>
-        </> 
-    )
-}
+        </>
+    );
+};
 
-export default Move
+export default Move;
